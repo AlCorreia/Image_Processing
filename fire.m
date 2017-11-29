bcd8eb7140cda25c3611682f5145413bb27c5a7b
AlCorreia <alvaro.hc.correia@gmail.com>
2017-11-29 16:27:35 -0200

    Blob detection

--- fire.m
+++ fire.m
@@ -9,20 +9,58 @@ hueThresholdLow = 0.10;
 hueThresholdHigh = 0.14;
 saturationThresholdLow = 0.4;
 saturationThresholdHigh = 1;
-valueThresholdLow = 0.8;
+valueThresholdLow = 0.7;
 valueThresholdHigh = 1.0;
+
+smallestAcceptableArea=500;
 
 hueMask = (h_pic >= hueThresholdLow) & (h_pic <= hueThresholdHigh);
 saturationMask = (s_pic >= saturationThresholdLow) & (s_pic <= saturationThresholdHigh);
 valueMask = (v_pic >= valueThresholdLow) & (v_pic <= valueThresholdHigh);
 
-coloredObjectsMask = uint8(hueMask & saturationMask & valueMask);
+filtered_image = uint8(double(pic_gray).*double(coloredObjectsMask));
+sobel_mask = imcomplement(edge(filtered_image, 'sobel', 0.07));
+
+structuringElement = strel('disk', 11);
+
+sobel_mask = imerode(sobel_mask, structuringElement);
+sobel_mask = imdilate(sobel_mask, structuringElement);
+
+coloredObjectsMask = uint8(hueMask & saturationMask & valueMask & sobel_mask);
+
+% Get rid of small objects.  Note: bwareaopen returns a logical.
+coloredObjectsMask = uint8(bwareaopen(coloredObjectsMask, smallestAcceptableArea));
+% subplot(3, 3, 1);
+% imshow(coloredObjectsMask, []);
+% fontSize = 13;
+% caption = sprintf('bwareaopen() removed objects\nsmaller than %d pixels', smallestAcceptableArea);
+% title(caption, 'FontSize', fontSize);
+
+% Smooth the border using a morphological closing operation, imclose().
+structuringElement = strel('disk', 2);
+coloredObjectsMask = imclose(coloredObjectsMask, structuringElement);
+% subplot(3, 3, 2);
+% imshow(coloredObjectsMask, []);
+% fontSize = 16;
+% title('Border smoothed', 'FontSize', fontSize);
+
+% Fill in any holes in the regions, since they are most likely red also.
+coloredObjectsMask = imfill(logical(coloredObjectsMask), 'holes');
+% subplot(3, 3, 3);
+% % imshow(coloredObjectsMask, []);
+% title('Regions Filled', 'FontSize', fontSize);
+
+
+coloredObjectsMask = uint8(coloredObjectsMask & sobel_mask);
+
 
 [meanHSV, areas, numberOfBlobs] = MeasureBlobs(coloredObjectsMask, h_pic, s_pic, v_pic);
 
 function [meanHSV, areas, numberOfBlobs] = MeasureBlobs(maskImage, hImage, sImage, vImage)
 try
 	[labeledImage, numberOfBlobs] = bwlabel(maskImage, 8);     % Label each blob so we can make measurements of it
+    coloredLabels = label2rgb (labeledImage, 'hsv', 'k', 'shuffle');
+    imshow(coloredLabels);
 	if numberOfBlobs == 0
 		% Didn't detect any blobs of the specified color in this image.
 		meanHSV = [0 0 0];
